e874182aa65bf4d4a419c3c3b1b7ff88d8bbb955
Jane-o-O-o-O <Jane-o-O-o-O@users.noreply.github.com>
2026-06-07 13:10:46 +0000

    chore(leaderboard): minor cleanup

--- src/evalarena/leaderboard.py
+++ src/evalarena/leaderboard.py
@@ -250,3 +250,18 @@ class _BaseHandler:
     def _teardown(self):
         """Cleanup resources."""
         self._metrics.flush()
+
+# [2026-06-07] Chore: update leaderboard
+# Version bump and minor cleanup
+__version_info__ = (1, 9, 29)
+__version__ = ".".join(map(str, __version_info__))
+
+# Updated configuration defaults
+_DEFAULT_CONFIG = {
+    "enabled": True,
+    "debug": False,
+    "max_retries": 3,
+    "timeout": 30,
+    "cache_size": 256,
+    "log_level": "INFO",
+}
